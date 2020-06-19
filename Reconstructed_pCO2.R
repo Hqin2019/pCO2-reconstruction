d9318da0e85b2762d637079f6f330580fcd0c360
Huan Qin <hqin@sdsu.edu>
2020-06-19 01:21:05 -0700

    Load the data and polish the code.

--- Reconstructed_pCO2.R
+++ Reconstructed_pCO2.R
@@ -7,91 +7,33 @@ library(ggplot2)
 library(readxl)
 library(scales)
 
-## Read remote sensing data from 2000 to 2014: Summer
-
-t1 = 2000
-t2 = 2014
-ngrid = 1040 #number of grid boxes 
-ntime = t2-(t1-1) #15 years
-rsname=1: ntime
-for (i in 1: ntime){
-  rsname[i] = paste("pCO2_rs_summer", 1999+i, ".txt", sep="")
-}
-
-### Generate a matrix as the data holder: 1040X15 for (i in 1:ntime)
-
-data_rs = matrix(0, nrow=ngrid, ncol=ntime)
-for (i in 1:ntime){
-  dat = read.table(rsname[i], header=TRUE)
-  data_rs[,i] = dat[,3]
-}
-
-## Data Pre-processing.
-
-### Remove the available values at the same observation point if there is NaN  
-
-for (i in 1:ntime) {
-  n = which(data_rs[ ,i] == "NaN")
-  data_rs[n,] <- NA
-}
-
-### Remove NA and obtain data with only values
-
-mar1 = is.na(data_rs[,3]) #This is from one of the RS data
-length(mar1)
-#[1] 1040  the number of True and False entries
-
-mar2 = which(mar1 == TRUE) #renders which positions with NaN
-length(mar2)
-#[1] 143  the number of NaN rows
-
-mar3 = which(mar1 == FALSE) #renders which positions with values
-length(mar3) 
-#[1] 897  the number of boxes with values
-#897 + 143 = 1040  = 40X26 grid boxes
-
-da0014 = na.omit(data_rs)#remove NaN and leave only values in the matrix da0014
-dim(da0014)
-#[1] 897   15  # 897 rows 
-#897 + 143 = 1040 rows total = 40X26
-
-### Lat and Lon of the grid
-data_rs_1=read.table(rsname[1], header=TRUE)
-latlon=data_rs_1[,1:2]
-dim(latlon)
-#[1] 1040    2
-latlon_val=latlon[mar3,]
-dim(latlon_val)
-#[1] 897   2  #latlon for 897 value boxes
-latlonnan=latlon[mar2,]
-dim(latlonnan)
-#[1] 143   2 #latlon for 143 NaN boxes
-
-RSdata = cbind(latlon, data_rs)
+## load remote sensing data and observed data.
+load(file="data/RSdata.RData")
+load(file="data/OBdata.RData")
+load(file="data/UNAN.RData")
 
 ## Climatology of Remote Sensing Data
 clim_rs=rowMeans(data_rs,na.rm=TRUE)
-
-Lat=seq(5.25, 24.75, len=40) #0.5-by-0.5 deg resolution
-Lon=seq(109.25,121.75,len=26) #0.5-by-0.5 deg resolution
-
 summary(clim_rs, na.rm=TRUE)
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
 #359.9   413.1   416.1   415.6   419.2   457.8     143 
 
 ## Standard Deviation of Remote Sensing Data
-
 sd_rs=rowSds(data_rs,na.rm=TRUE)
-
 summary(sd_rs, na.rm=TRUE)
 #Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
 #1.096   4.297   5.464   5.931   7.088  30.197     143 
 
+t1 = 2000
+t2 = 2014
+ngrid = 1040 #number of grid boxes 
+ntime = t2-(t1-1) #15 years
+Lat=seq(5.25, 24.75, len=40) #0.5-by-0.5 deg resolution
+Lon=seq(109.25,121.75,len=26) #0.5-by-0.5 deg resolution
 labels <- c("(a)","(b)","(c)","(d)","(e)","(f)","(g)","(h)","(i)","(j)","(k)","(l)","(m)","(n)","(o)")
 years_rs <- seq(t1, t2, length=ntime)
 
 ## Compute EOFs: The EOF patterns show important spatial patterns of pCO2
-
 clim_rs_std = ( data_rs - clim_rs) / sd_rs #Compute the standardized anomalies
 dim(clim_rs_std)
 #[1] 1040   15 #all the 1040 grid boxes
@@ -102,24 +44,11 @@ dim(clim_rs_std_val)
 #897 + 143 = 1040 rows total = 40X26 
 
 ### EOF matrices with NaN for map plotting.
-
 svd00=svd(clim_rs_std_val) #SVD for the matrix w/o NaN
 #SVD does not work for a matrix with NaN
 u00=svd00$u
 v00=svd00$v
 d00=svd00$d
-
-UNaN=matrix(0,nrow=1040, ncol=ntime)
-UNaN[mar2,]=NaN #mar2 is the NaN rows
-
-for(i in 1:897){
-  for (j in 1: ntime ){
-    UNaN[mar3[i],j] = u00[i,j] #mar3 is the value rows
-  }
-}
-
-var=d00^2/ntime
-cvar=cumsum(var)
 
 ##  Write the EOF data with header
 eofnames=1:ntime
@@ -131,53 +60,9 @@ for (i in 1:ntime){
 EOFs=u00
 colnames(EOFs) <- eofnames
 
-# Summer pCO2 data analysis for insitu data 2000-2017
-
-## Read observed data from 2000 to 2014: Summer
-
-### Gridding by the SOG method
-
-### Step 1: Make a list of file names for the summer obs data files
-
-obsnames=list.files(path = "." , pattern = "pCO2_variance_summer" )
-length(obsnames)
-#[1] 13 The Summer has 13 files
-
-### Step 2: Read the summer obs data into a single file
-
-t1=1
-t2=length(obsnames)
-ngrid=1040
-
-#Generate a matrix as the data holder: 1040X13 for (i in 1:ntime)
-data_obs=matrix(0, nrow=ngrid, ncol=t2)
-
-for (i in t1:t2){
-  dat=read.table(obsnames[i], header=TRUE)
-  data_obs[,i] =dat[,8]
-}
-
-dim(data_obs)
-#[1] 1040   13
-
-## Outlier Detection 
-
-data_obs1=matrix(NA, nrow=ngrid, ncol=t2)
-min_obs=c()
-
-for (i in t1:t2){
-  sd_obs <- sd(data_obs, na.rm = TRUE)
-  min_obs[i] <- mean(data_obs[,i], na.rm = TRUE) - 3*sd_obs 
-  data_obs1[which(data_obs[,i] > min_obs[i]), i ] <- data_obs[which(data_obs[,i] > min_obs[i]),i]
-}
-
-obslatlon=cbind(latlon,data_obs1)
-
 years_obs = c(2000,2001,2004:2009,2012,2014:2017)
 
-
 # Reconstruction
-
 ## Compute the standardized anomalies of the observed data with RS climatology and standard deviation
 
 data_obs_anom = (data_obs1 - clim_rs) / sd_rs
@@ -195,7 +80,7 @@ n_ocean
 
 n_mode=c()
 
-for (i in t1:t2) {
+for (i in 1:13) {
   v=which(complete.cases(data_ocean[,i])) #the boxes with data
   n_mode[i]=length(v)-1 
 }
@@ -207,7 +92,7 @@ k2 = 8 #set the number of modes to be 8 except the first year
 
 #Generate the lm formula 
 
-recon=matrix(0,nrow=n_ocean,ncol=t2)
+recon=matrix(0,nrow=n_ocean,ncol=13)
 dim(recon)
 #[1] 897   13 #To hold the recon result
 
@@ -225,7 +110,7 @@ for (i in 1) {
   recon[,i]=res%*%coe
 }
 
-for (i in 2:t2) {
+for (i in 2:13) {
   v=which(complete.cases(data_ocean[,i])) #the boxes with data
   #u=data_ocean[v,i]
   datr=data_ocean[v,i] #Observed data
@@ -253,9 +138,6 @@ for(i in 2:896){
       reconfield_smr[i, j ]=(reconfield_smr[ i-1, j ]+ reconfield_smr[ i, j ]+ reconfield_smr[ i+1, j ])/3
   }
 }
-
-
-reconlatlon_smr=cbind(latlon_val,reconfield_smr)
 
 ## Figure 7. Reconstructed pCO2 fields in the SCS. 
 
