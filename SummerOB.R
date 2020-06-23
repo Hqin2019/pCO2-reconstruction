e6857f0414be9d2933fc6a446da9069f637a0460
Huan Qin <hqin@sdsu.edu>
2020-06-22 20:58:43 -0700

    Add plot.new().

--- SummerOB.R
+++ SummerOB.R
@@ -20,10 +20,9 @@ labels <- c("(a)","(b)","(c)","(d)","(e)","(f)","(g)","(h)","(i)","(j)","(k)","(
 years_obs = c(2000,2001,2004:2009,2012,2014:2017)
 
 ## set up an empty frame, then add points one by one
-plot.new()
 par(bg = "white") # ensure the background color is white
 ani.record(reset = TRUE) # clear history before recording
-
+plot.new()
 for (i in 1:13) {
   mapmat=matrix(data_obs1[,i],nrow=26)
   mapmat=pmax(pmin(mapmat,480),220)
