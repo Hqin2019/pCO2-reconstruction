7f8324925791799eb6caf37bfbb67b0cc10677ad
Huan Qin <hqin@sdsu.edu>
2020-12-21 23:49:32 -0800

    Update Leave-one-out Cross-validation

--- Reconstructed_TS.R
+++ Reconstructed_TS.R
@@ -70,7 +70,7 @@ layout(matrix(c(1,2), 2, 1, byrow = TRUE))
 par(mar=c(1,6,2,2))
 time1 = years_obs
 plot(time1,space_ave,type="b", xlim=c(2000,2018), ylim=c(350,440), yaxt="n", ann = FALSE, cex=1.5, cex.axis = 1.5)
-mtext(expression(paste(italic(p),CO[2], ~"(", mu, "atm)")), side= 2, cex= 1.5, line= 3.5 )
+mtext(expression(paste(italic(p),CO[2], ~"(", mu, "atm)")), side= 2, cex= 1.5, line= 3.5)
 axis(2,las=2, cex.axis = 1.5)
 abline(lm(space_ave ~ time1), col='red',lwd=2,lty=2) #Plot the trend line
 legend( time1[1], 439, text.col='red', legend = expression(paste("Temporal trend: 2.383",~mu, "atm" ,~"per year")),
@@ -84,6 +84,7 @@ legend( 2000, 425,text.col='red',cex=1.5,
 legend("topleft", text.col='black', legend = NA,
        title = expression(paste("(a) Spatial Averages of the Reconstructed ", italic(p), CO[2], " Data")),
        bty="n",cex=1.5)
+
 
 par(mar=c(5.1,6,2,2))
 plot(x, y ,  yaxt='n', ann = FALSE,xlim=c(min(as.Date('2000/01/01')),max(as.Date('2018/01/01'))), ylim=c(335,410), cex = 1.5, cex.axis = 1.5)
@@ -103,5 +104,6 @@ legend(x[1], 394, text.col='red',
 legend("topleft", text.col='black', legend = NA,
        title = expression(paste("(b) Sea Surface ", italic(p), CO[2], " at Station HOT")),
        bty="n",cex=1.5, pt.cex=0)
+
 
 
